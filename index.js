0affc218acf74968eb3d728cb304353b45626e3f
Tksi <Tksi@users.noreply.github.com>
2021-10-29 16:46:40 +0900

    [upd] to ESmodule

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const { ApolloServer } = require('apollo-server');
+import { ApolloServer } from 'apollo-server';
 
+let _id = 0; //idのインクリメント用
 const photos = [];
 
 // スキーマ : データの要件
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `;
 
@@ -17,11 +27,16 @@ const typeDefs = `
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
   Mutation: {
     postPhoto: (parent, args) => {
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     },
   },
 };
